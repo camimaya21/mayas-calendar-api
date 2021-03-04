c150c6aa9ea0b571be2573203db7e47c00fd2d4d
camimaya21 <camilamaya21@gmail.com>
2021-03-04 23:40:19 +0100

    chore: added config module and service to set process env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigKeys } from './config/config.keys';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number | string;
+
+  constructor(private readonly _configService: ConfigService) {
+    AppModule.port = _configService.get(ConfigKeys.PORT);
+  }
+}
