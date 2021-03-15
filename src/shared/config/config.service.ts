04455a2b7b4f137966f2b81b55305529773d78b3
Camila Maya S <31632069+camimaya21@users.noreply.github.com>
2021-03-15 23:45:00 +0100

    fix: startDev script (#11)
    * test: mocks added
    
    * tests: eventService tests and fixed update and delete methods
    
    * test: events repository
    
    * test: events controller
    
    * test: delete unused var
    
    * chore: added db scripts and updated readme (#6)
    
    * chore: seeding db with prisma client (#8)
    
    * chore: add startDev script, dockerfiles and update Readme
    
    * fix: startDev script and .env path in configService

--- src/shared/config/config.service.ts
+++ src/shared/config/config.service.ts
@@ -8,7 +8,7 @@ export class ConfigService {
     const isDevelopmentEnv = process.env.NODE_ENV !== 'production';
 
     if (isDevelopmentEnv) {
-      const envFilePath = `${__dirname}/../../../.env`;
+      const envFilePath = `./.env`;
       const existsPath = fs.existsSync(envFilePath);
 
       if (!existsPath) {
--- startDev.sh
+++ startDev.sh
@@ -1,5 +1,5 @@
 #!bin/sh
-echo "PORT=5000\nDATABASE_URL="postgresql://postgres:password@localhost:5445/mayascalendar"" > .env
+echo "PORT=5000\nDATABASE_URL=\"postgresql://postgres:password@localhost:5445/mayascalendar\"" > .env
 docker rm mayas-postgres
 docker run -p 5445:5432 -d -e POSTGRES_PASSWORD=password --name mayas-postgres postgres
 yarn install
